3406cd867506e12ad1c42594c8a1a190ab94514f
nasiru sodiq <nassodiq06@gmail.com>
2025-08-28 18:30:48 +0100

    i just work on my javascript

--- website/js/main.js
+++ website/js/main.js
@@ -5,9 +5,17 @@ const owner = "Nas construction company";
 document.getElementById("copyright").innerHTML = 
 `&copy; ${year} ${owner}. All rights reserved.`;
             
-    console.log("document loaded!");
-};
+    
+
+document.getElementById("showpicbutton").addEventListener("click", function() {
+  document.getElementsByClassName("sideeq")[0].classList.add("make-it-visible");
+    document.getElementsByClassName("sideeq")[1].classList.add("make-it-visible");
+    document.getElementsByClassName("sideeq")[2].classList.add("make-it-visible");
+
+});
 
 
 
                    
+console.log("document loaded!");
+};
